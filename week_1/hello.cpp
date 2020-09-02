1ed949b9a96bfa0a8e4b74aff98b03d81e3f8c52
Microzenas <microzenas@gmail.com>
2020-09-01 17:13:35 -0700

    # Hello World # Week1

---
+++ week_1/hello.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>  //c
+#include <iostream> // c++
+int main()
+{
+    std::cout << "hello world" << std::endl; //c++
+    printf("hello world");                   //c
+    return 0;
+}
