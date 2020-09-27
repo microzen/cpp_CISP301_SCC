a008ae7caaaa1f3a3bd18a9a0bfa258f9852d40c
Microzenas <microzenas@gmail.com>
2020-09-27 00:11:15 -0700

    # Julian day number for Lab Assignment 3

--- week_4/julianDayNumber.cpp
+++ week_4/julianDayNumber.cpp
@@ -5,30 +5,82 @@
  * File Created: Saturday September 19th 2020 07:22 pm
  * Author: Microzenas(Microzenas@gmail.com)
  * -----
- * Last Modified: Saturday September 19th 2020 07:49 pm
+ * Last Modified: Sunday September 27th 2020 12:09 am
  * Modified By: Microzenas
  * -----------------------------------------------------------------------
  */
 #include <iostream>
-int julianDayNumber(int year, int month, int day);
+void julianDayNumber(int year, int month, int day, int &jdn);
+void inputDate(int &year, int &month, int &day);
+void printResults(int jdn);
+
 int main(int argc, char *argv[])
 {
-   const int year = 2020;
-   const int month = 9;
-   const int day = 19;
-   int jdn = julianDayNumber(year, month, day);
-   std::cout << "JDN is:" << jdn
-             << "\nDOW is:" << (jdn+1) % 7
-             << std::endl;
+   // using for recording date
+   int year, month, day;
+   // using for recording the result for JDN
+   int jdn;
+
+   inputDate(year, month, day);
+   julianDayNumber(year, month, day, jdn);
+   printResults(jdn);
 
    return 0;
 }
-int julianDayNumber(int year, int month, int day)
+//
+// A input date module by user.
+//
+void inputDate(int &year, int &month, int &day)
+{
+   std::cout << "Enter number of year\t: ";
+   std::cin >> year;
+   std::cout << "Enter number of month\t: ";
+   std::cin >> month;
+   std::cout << "Enter number of day\t: ";
+   std::cin >> day;
+}
+//
+// A processing module that gets julian day number by a data.
+//
+void julianDayNumber(int year, int month, int day, int &jdn)
 {
    int a = (14 - month) / 12;
    int m = (month - 3) + (12 * a);
    int y = year + 4800 - a;
    int leap_days = (y / 4) - (y / 100) + (y / 400);
-   int jdn = day + ((153 * m) + 2) / 5 + (365 * y) + leap_days - 32045;
-   return jdn;
+   jdn = day + ((153 * m) + 2) / 5 + (365 * y) + leap_days - 32045;
+}
+//
+// An output module that displays the JDN and DOW
+//
+void printResults(int jdn)
+{
+   // using for recording the week of string.
+   std::string week;
+   switch (jdn % 7)
+   {
+   case 0:
+      week = "Monday";
+      break;
+   case 1:
+      week = "Tuesday";
+      break;
+   case 2:
+      week = "Wednesday";
+      break;
+   case 3:
+      week = "Thursday";
+      break;
+   case 4:
+      week = "Friday";
+      break;
+   case 5:
+      week = "Saturday";
+      break;
+   case 6:
+      week = "Sunday";
+      break;
+   }
+   std::cout << "JDN is:" << jdn
+             << "\nDOW is:" << week << std::endl;
 }
